76d3c0a4c21e71a12c927ca4fe70035b9563cb66
CJ <cjrodriguez2448@gmail.com>
2024-01-26 13:58:21 -0800

    commit changes

--- 4 kyu/Reduced Fractions.py
+++ 4 kyu/Reduced Fractions.py
@@ -1,10 +1,19 @@
-import math
+def euler_totient_function(n):
+    if n <= 0:
+        return 0
+    result = n
+    
+    i = 2
+    while i * i <= n:
+        if n % i == 0:
 
-n = 500000003
-count = 0
+            while n % i == 0:
+                n //= i
+            result -= result // i
+        i += 1
+    
 
-for i in range(1, n):
-    if  math.gcd(i, n) == 1:
-        count += 1
-
-print(count)
+    if n > 1:
+        result -= result // n
+    
+    return result
